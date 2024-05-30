ee7a612059d8cad65ee7986a8732a7fadb127d6b
r <49694912+RicardFabien@users.noreply.github.com>
2024-05-30 23:39:38 +0200

    feat: init component

---
+++ frontend/src/Chat/Chat.js
@@ -0,0 +1,9 @@
+function Chat(){
+    return (
+        <div className="Chat App-part">
+            Chat
+        </div>
+    )
+}
+
+export default Chat;
---
+++ frontend/src/Game/Game.js
@@ -0,0 +1,10 @@
+function Game() {
+    return (
+        <div className="Game App-part">
+            game
+        </div>
+    )
+        
+}
+
+export default Game;
