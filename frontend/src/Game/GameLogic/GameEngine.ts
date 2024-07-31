1a06998b762b53cc5d45280e23320654bdd7d478
r <49694912+RicardFabien@users.noreply.github.com>
2024-07-31 17:36:58 +0200

    refactor: factorisation

--- frontend/src/Game/GameLogic/GameEngine.ts
+++ frontend/src/Game/GameLogic/GameEngine.ts
@@ -1,31 +1,8 @@
 class GameEngine{
-
-}
-
-class GameMap{
-
-    grid: Square[][];
+    map: GameMap;
 
     constructor(gridSize: number){
-        // makes a bidimensionnal array filled with Square objects
-        // The size of the bidimensionnal array is gridSize
-        // So gridsize 3 would be a 3x3 grid
-        this.grid = 
-        Array.from( {length:gridSize}, ()=> {
-                return Array.from({length:gridSize}, ()=> {
-                   return new Square()
-                })
-            });
-        
+        this.map = new GameMap(gridSize);
     }
 
-}
-
-class Square{
-    revealedWall :{
-        up:boolean,
-        down:boolean,
-        left: boolean,
-        right: boolean
-    } = {up:false,down:false, left:false, right:false}
 }
---
+++ frontend/src/Game/GameLogic/GameMap.ts
@@ -0,0 +1,28 @@
+
+class GameMap{
+
+    grid: Square[][];
+
+    constructor(gridSize: number = 3){
+        // makes a bidimensionnal array filled with Square objects
+        // The size of the bidimensionnal array is gridSize
+        // So gridsize 3 would be a 3x3 grid
+        this.grid = 
+        Array.from( {length:gridSize}, ()=> {
+                return Array.from({length:gridSize}, ()=> {
+                   return new Square()
+                })
+            });
+        
+    }
+
+}
+
+class Square{
+    revealedWall :{
+        up:boolean,
+        down:boolean,
+        left: boolean,
+        right: boolean
+    } = {up:false,down:false, left:false, right:false}
+}
