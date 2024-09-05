1e10d49df3b8576da440f65d8877b37af5e72344
r <49694912+RicardFabien@users.noreply.github.com>
2024-09-05 18:27:00 +0200

    feat: tracks and wall are visibly shown

--- frontend/src/Game/GamePanel.tsx
+++ frontend/src/Game/GamePanel.tsx
@@ -33,45 +33,40 @@ function Row(props: any) {
   return <div className={"Row"}>{props.children}</div>;
 }
 
-// const containerStyle = {
-//   display: "flex",
-//   alignItems: "left",
-//   "flex-wrap": "wrap",
-// };
-
 const containerStyle = {
   display: "grid",
   "grid-template-columns": "50% 50%",
   "grid-template-rows": "50% 50%",
 };
-
-const squarePartStyle = {
-  // width: "50%",
-  // height: "50%",
-};
+var quarterBaseStyle = {};
 
 function Square(props: any) {
   const revealedWall = props.children.revealedWall;
   const tracks = props.children.tracks;
 
-  var quarterSquareStyle = squarePartStyle;
-
   var div1Style,
     div2Style,
     div3Style,
-    div4Style: {} = quarterSquareStyle;
+    div4Style: {} = quarterBaseStyle;
 
   // If the flag for the track is true, add a border that represent that tracks
   // If tracks.down is true, a bottom track will be shown
   // If tracks.left is false, the left track will NOT be shown
-  if (tracks.up) div1Style = { ...quarterSquareStyle, borderRight: "solid" };
-  if (tracks.right)
-    div2Style = { ...quarterSquareStyle, borderBottom: "solid" };
-  if (tracks.left) div3Style = { ...quarterSquareStyle, borderTop: "solid" };
-  if (tracks.down) div4Style = { ...quarterSquareStyle, borderLeft: "solid" };
+  if (tracks.up) div1Style = { ...quarterBaseStyle, borderRight: "solid" };
+  if (tracks.down) div4Style = { ...quarterBaseStyle, borderLeft: "solid" };
+  if (tracks.left) div3Style = { ...quarterBaseStyle, borderTop: "solid" };
+  if (tracks.right) div2Style = { ...quarterBaseStyle, borderBottom: "solid" };
+
+  var newContainerStyle = {
+    ...containerStyle,
+    borderTop: revealedWall.up ? "solid red" : "",
+    borderBottom: revealedWall.down ? "solid red" : "",
+    borderLeft: revealedWall.left ? "solid red" : "",
+    borderRight: revealedWall.right ? "solid red" : "",
+  };
 
   return (
-    <div className={"Square"} style={containerStyle}>
+    <div className={"Square"} style={newContainerStyle}>
       <div style={div1Style}></div>
       <div style={div2Style}></div>
       <div style={div3Style}></div>
